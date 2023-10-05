38a71276e59b51660be36b1dd7ea24f664204340
DESKTOP-RLS3OA7\leo <leopoldo.piatti@enti.cat>
2023-10-05 13:21:47 +0200

    05/10

--- Assets/Scripts/BrokenTargetController.cs
+++ Assets/Scripts/BrokenTargetController.cs
@@ -4,7 +4,7 @@ public class BrokenTargetController : MonoBehaviour
 {
     public float despawnTime = 5f;  // Tiempo antes de despawnear las piezas individuales
     public Vector3 spawnRotation;  // Rotación al spawnear
-
+    //asd
     private void Start()
     {
         // Aplicar la rotación al spawnear
